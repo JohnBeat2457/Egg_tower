cccf82553cf2e3746cf01e64d3f623d0de5ac328
Nogebeat <james.gbetchedji@epitech.eu>
2024-03-31 18:20:47 +0100

    add netlify identity

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const mongoose = require('mongoose');
+const fetch = require('node-fetch');
 
 mongoose.connect('mongodb+srv://nogebeat:<password>@cluster0.mjz3juq.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true});
 const Name = mongoose.model('Name', new mongoose.Schema({ name: String }));
-
 
 app.use(express.static('public')); // Pour servir les fichiers statiques
 
@@ -22,6 +22,19 @@ app.get('/get-names', async (req, res) => {
     res.send(names);
 });
 
+app.post('/auth', async (req, res) => {
+    const { token } = req.body;
+    const response = await fetch('https://api.netlify.com/api/v1/me', {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const user = await response.json();
+    if (user.code === 200) {
+        res.send({ status: 'OK', user });
+    } else {
+        res.send({ status: 'error', message: 'Authentication failed' });
+    }
+});
 
 app.listen(3000, () => console.log('Server started on port 3000'));
-
