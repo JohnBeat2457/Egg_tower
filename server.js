4e63188f07682dadc6f470f683a80c25fa06d75c
Nogebeat <james.gbetchedji@epitech.eu>
2024-03-31 17:03:15 +0100

    change server.js for publish

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://nogebeat:<password>@cluster0.mjz3juq.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true});
+const Name = mongoose.model('Name', new mongoose.Schema({ name: String }));
+
 
 app.use(express.static('public')); // Pour servir les fichiers statiques
 
 app.use(express.json());
 
-app.post('/save-name', (req, res) => {
-    const name = req.body.name;
-    fs.appendFile('/site/name.txt', name + '\n', err => {
-        if (err) throw err;
-        console.log('Saved!');
-    });
+app.post('/save-name', async (req, res) => {
+    const name = new Name({ name: req.body.name });
+    await name.save();
     res.send({ status: 'OK' });
 });
 
-app.get('/get-names', (req, res) => { // Nouvelle route GET
-    fs.readFile('/site/name.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.send(data);
-    });
+app.get('/get-names', async (req, res) => {
+    const names = await Name.find();
+    res.send(names);
 });
 
+
 app.listen(3000, () => console.log('Server started on port 3000'));
+
