07489500ba894e4cabc9b7a6e085856b689e2ede
Nogebeat <james.gbetchedji@epitech.eu>
2024-03-31 16:06:54 +0100

    add get-names

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.use(express.static('site'));
+app.use(express.static('public')); // Pour servir les fichiers statiques
 
 app.use(express.json());
 
@@ -13,6 +13,13 @@ app.post('/save-name', (req, res) => {
         console.log('Saved!');
     });
     res.send({ status: 'OK' });
+});
+
+app.get('/get-names', (req, res) => { // Nouvelle route GET
+    fs.readFile('./site/name.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
 });
 
 app.listen(3000, () => console.log('Server started on port 3000'));
