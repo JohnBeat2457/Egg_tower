9835165f7089bcb0c151e3255a80f6db1cdc7230
Nogebeat <james.gbetchedji@epitech.eu>
2024-04-01 01:05:06 +0100

    encore un nouveau push

--- server.js
+++ server.js
@@ -1,30 +1,55 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 
-app.use(express.static('site'));
 
+app.use(express.static('site'));
 app.use(express.json());
+
+const fs = require('fs');
 
 app.post('/save-name', (req, res) => {
     const name = req.body.name;
-    fs.appendFile('./site/name.txt', name + '\n', err => {
+    fs.appendFile('/site/name.txt', name + '\n', err => {
         if (err) throw err;
-        console.log('Saved!');
+        console.log('Name saved:', name);
     });
     res.send({ status: 'OK' });
 });
 
 app.get('/get-names', (req, res) => {
-    fs.readFile('./site/name.txt', 'utf8', (err, data) => {
+    fs.readFile('/site/name.txt', 'utf8', (err, data) => {
         if (err) throw err;
-        res.send(data);
+        res.send(data.split('\n').filter(name => name !== ''));
     });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+// ffd
+
+function saveName() {
+    var name = document.getElementById('nameInput').value;
+    fetch('https://eggtower2noge.netlify.app/save-name', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+// dsvf
+
+fetch('https://eggtower2noge.netlify.app/get-names')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(name => {
+            // Affichez chaque nom dans une liste sur votre site web
+            console.log(name);
+        });
+    });
